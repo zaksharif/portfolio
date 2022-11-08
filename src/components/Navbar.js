6c8c0f9326a3aef24abfadedaa40143eb2742eb1
zaksharif <Sharif_sharif07@yahoo.com>
2022-11-07 22:32:11 -0600

    Deployed website

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,12 +21,12 @@ const Navbar = () => {
     window.removeEventListener("scroll", changeColor);
     return (
         <div className={color ? "header header-bg" : "header"}>
-            <Link to="/">
+            <Link to="/portfolio">
                 <h1>Portfolio</h1>
             </Link>
             <ul className={click ? "nav-menu active" : "nav-menu"}>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/portfolio">Home</Link>
                 </li>
                 <li>
                     <Link to="/project">Project</Link>
